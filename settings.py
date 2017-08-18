792ad1b77fbf25d030703ab8e773957862319444
mmmsr <mm.xjr400s@gmail.com>
2017-08-18 07:17:59 +0000

    initial commit

--- settings.py
+++ settings.py
@@ -4,9 +4,9 @@ import os
 
 MONGO_URL = os.environ.get('MONGO_URL')
 if not MONGO_URL:
-    MONGO_URL = "mongodb://<dbuser>:<dbpassword>@xxxxxxxx.mlab.com:xxxxx/hogehoge"
+    MONGO_URL = 'mongodb://admin:admin@ds127429.mlab.com:27429/markovchain-sandobox-db'
 
-MONGO_DB_NAME = 'hogehoge'
+MONGO_DB_NAME = 'markovchain-sandobox-db'
 
 DEFAULT_KEYWORD = 'プロテイン'
 
